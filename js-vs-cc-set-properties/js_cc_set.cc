5fa5a63930479e2f226251c5f7f80651b76df8cb
Trevor Norris <trev.norris@gmail.com>
2013-07-07 21:43:17 -0700

    Initial upgrade to v8 3.20.x
    This is the initial API upgrade to v8 3.20.x. There's still a lot going
    on, and hopefully performance fixes will be in soon.

--- js-vs-cc-set-properties/js_cc_set.cc
+++ js-vs-cc-set-properties/js_cc_set.cc
@@ -1,10 +1,15 @@
 #include <v8.h>
 #include <node.h>
 
+#define FN_PREP(args)                    \
+  Isolate* isolate = args.GetIsolate();  \
+  HandleScope scope(isolate);            \
+  do {} while(0)
 
-using v8::Arguments;
+
 using v8::Boolean;
 using v8::Function;
+using v8::FunctionCallbackInfo;
 using v8::FunctionTemplate;
 using v8::Handle;
 using v8::HandleScope;
@@ -17,7 +22,6 @@ using v8::String;
 using v8::Value;
 
 
-Isolate* isolate;
 Persistent<Function> p_setprop_fn;
 Persistent<Function> p_setobj_fn;
 Persistent<Object> p_empty_obj;
@@ -25,29 +29,13 @@ Persistent<String> p_length_sym;
 Persistent<String> p_type_sym;
 Persistent<String> p_used_sym;
 
-
-Handle<Value> SetInCCSlow(const Arguments& args) {
-  HandleScope scope(isolate);
-
-  size_t iter = args[0]->Uint32Value();
-  Local<Object> obj;
-  Local<Number> arg0 = args[1]->ToNumber();
-  Local<String> arg1 = args[2]->ToString();
-  Local<Boolean> arg2 = args[3]->ToBoolean();
-
-  for (size_t i = 0; i < iter; i++) {
-    obj = Object::New();
-    obj->Set(String::NewFromUtf8(isolate, "length"), arg0);
-    obj->Set(String::NewFromUtf8(isolate, "type"), arg1);
-    obj->Set(String::NewFromUtf8(isolate, "used"), arg2);
-  }
-
-  return scope.Close(obj);
-}
+static const uint8_t* length_sym;
+static const uint8_t* type_sym;
+static const uint8_t* used_sym;
 
 
-Handle<Value> SetInCCSym(const Arguments& args) {
-  HandleScope scope(isolate);
+void SetInCCSlow(const FunctionCallbackInfo<Value>& args) {
+  FN_PREP(args);
 
   size_t iter = args[0]->Uint32Value();
   Local<Object> obj;
@@ -57,17 +45,37 @@ Handle<Value> SetInCCSym(const Arguments& args) {
 
   for (size_t i = 0; i < iter; i++) {
     obj = Object::New();
-    obj->Set(p_length_sym, arg0);
-    obj->Set(p_type_sym, arg1);
-    obj->Set(p_used_sym, arg2);
+    obj->Set(String::NewFromOneByte(isolate, length_sym), arg0);
+    obj->Set(String::NewFromOneByte(isolate, type_sym), arg1);
+    obj->Set(String::NewFromOneByte(isolate, used_sym), arg2);
   }
 
-  return scope.Close(obj);
+  args.GetReturnValue().Set(obj);
 }
 
 
-Handle<Value> SetInJS(const Arguments& args) {
-  HandleScope scope(isolate);
+void SetInCCSym(const FunctionCallbackInfo<Value>& args) {
+  FN_PREP(args);
+
+  size_t iter = args[0]->Uint32Value();
+  Local<Object> obj;
+  Local<Number> arg0 = args[1]->ToNumber();
+  Local<String> arg1 = args[2]->ToString();
+  Local<Boolean> arg2 = args[3]->ToBoolean();
+
+  for (size_t i = 0; i < iter; i++) {
+    obj = Object::New();
+    obj->Set(Local<String>::New(isolate, p_length_sym), arg0);
+    obj->Set(Local<String>::New(isolate, p_type_sym), arg1);
+    obj->Set(Local<String>::New(isolate, p_used_sym), arg2);
+  }
+
+  args.GetReturnValue().Set(obj);
+}
+
+
+void SetInJS(const FunctionCallbackInfo<Value>& args) {
+  FN_PREP(args);
 
   size_t iter = args[0]->Uint32Value();
   Local<Object> obj;
@@ -78,15 +86,15 @@ Handle<Value> SetInJS(const Arguments& args) {
   for (size_t i = 0; i < iter; i++) {
     obj = Object::New();
     Local<Value> argv[3] = { arg0, arg1, arg2 };
-    p_setprop_fn->Call(obj, 3, argv);
+    Local<Function>::New(isolate, p_setprop_fn)->Call(obj, 3, argv);
   }
 
-  return scope.Close(obj);
+  args.GetReturnValue().Set(obj);
 }
 
 
-Handle<Value> SetInJSObj(const Arguments& args) {
-  HandleScope scope(isolate);
+void SetInJSObj(const FunctionCallbackInfo<Value>& args) {
+  FN_PREP(args);
 
   size_t iter = args[0]->Uint32Value();
   Local<Value> obj;
@@ -96,25 +104,23 @@ Handle<Value> SetInJSObj(const Arguments& args) {
 
   for (size_t i = 0; i < iter; i++) {
     Local<Value> argv[3] = { arg0, arg1, arg2 };
-    obj = p_setobj_fn->Call(p_empty_obj, 3, argv);
+    Local<Function> fn = Local<Function>::New(isolate, p_setobj_fn);
+    obj = fn->Call(Local<Object>::New(isolate, p_empty_obj), 3, argv);
   }
 
-  return scope.Close(obj);
+  args.GetReturnValue().Set(obj);
 }
 
 
-Handle<Value> SetupCC(const Arguments& args) {
-  p_setprop_fn = Persistent<Function>::New(isolate,
-                                           args[0].As<Function>());
-  p_setobj_fn = Persistent<Function>::New(isolate,
-                                          args[1].As<Function>());
-  return Undefined(isolate);
+void SetupCC(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  p_setprop_fn.Reset(isolate, args[0].As<Function>());
+  p_setobj_fn.Reset(isolate, args[1].As<Function>());
 }
 
 
 void Initialize(Handle<Object> target) {
-  isolate = Isolate::GetCurrent();
-
+  Isolate* isolate = Isolate::GetCurrent();
   HandleScope scope(isolate);
 
   target->Set(String::NewFromUtf8(isolate, "setInCCSlow"),
@@ -128,16 +134,14 @@ void Initialize(Handle<Object> target) {
   target->Set(String::NewFromUtf8(isolate, "setupCC"),
               FunctionTemplate::New(SetupCC)->GetFunction());
 
-  p_empty_obj = Persistent<Object>::New(isolate, Object::New());
+  length_sym = reinterpret_cast<const uint8_t*>("length");
+  type_sym = reinterpret_cast<const uint8_t*>("type");
+  used_sym = reinterpret_cast<const uint8_t*>("used");
 
-  Local<String> length = String::NewFromUtf8(isolate, "length");
-  p_length_sym = Persistent<String>::New(isolate, length);
-
-  Local<String> type = String::NewFromUtf8(isolate, "type");
-  p_type_sym = Persistent<String>::New(isolate, type);
-
-  Local<String> used = String::NewFromUtf8(isolate, "used");
-  p_used_sym = Persistent<String>::New(isolate, used);
+  p_empty_obj.Reset(isolate, Object::New());
+  p_length_sym.Reset(isolate, String::NewFromOneByte(isolate, length_sym));
+  p_type_sym.Reset(isolate, String::NewFromOneByte(isolate, type_sym));
+  p_used_sym.Reset(isolate, String::NewFromOneByte(isolate, used_sym));
 }
 
 
